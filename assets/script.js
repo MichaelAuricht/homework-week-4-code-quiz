399f09e95f731d0f6069ea3179e849c5b2a1b56a
Michael Auricht <michaelauricht@live.com.au>
2022-03-23 22:10:56 +1100

    redid html and js as hit a wall without arrays

--- assets/script.js
+++ assets/script.js
@@ -1,112 +1,163 @@
 
 var timeEl = document.getElementById("countdown");
 var mainEl = document.getElementById("main");
-var secondsLeft = 3;
+var pageoneEl = document.getElementById("pageone");
+var questionanswerareaEl = document.getElementById("questionanswerarea");
+var pagequestionEl = document.getElementById("pagequestion");
+var pageanswersEl = document.getElementById("pageanswers");
+var resultareaEl = document.getElementById("resultarea");
+var highscoreareaEl = document.getElementById("highscorearea");
+var finalscoreEl = document.getElementById("finalscore");
+
+
+var secondsLeft = 60;
 var startQuizEl = document.getElementById("startquiz");
+startQuizEl.addEventListener("click", startQuiz);
 
-var pageTwoT = document.getElementById("alerts-t")
-var pageThreeT = document.getElementById("parent-t");
-var pageFourT = document.getElementById("all-t");
-var pageFiveT = document.getElementById("quotes-t");
-var pageSixT = document.getElementById("js-t");
-
-var pageTwoF = document.getElementById("strings-f") || document.getElementById("booleans-one-f") || document.getElementById("numbers-one-f");
-var pageThreeF = document.getElementById("quotes-f") || document.getElementById("curly-one-f") || document.getElementById("square-one-f");
-var pageFourF = document.getElementById("numbers-two-f") || document.getElementById("arrays-f") || document.getElementById("booleans-two-f");
-var pageFiveF = document.getElementById("commas-f") || document.getElementById("curly-two-f") || document.getElementById("parent-f");
-var pageSixF = document.getElementById("terminal-f") || document.getElementById("leaps-f") || document.getElementById("console-f");
+var current_page_number = 0;
+var global_score = 0;
 
 
-function pageTwo() {
-    var pageTwoEl = document.getElementById("pagetwo");
-    if (pageTwoEl.style.display = "none") {
-        pageTwoEl.style.display = "block";
-    } else {
-        pageTwoEl.style.display = "none";
-    }
-    var pageOneEl = document.getElementById("pageone");
-    if (pageOneEl.style.display = "block") {
-        pageOneEl.style.display = "none";
-    }
+var question_1 = {answer:2, answeroptions: ["strings", "booleans", "alerts", "numbers"], question: "1. Commonly used data types DO NOT include:"};
+var question_2 = {answer:2, answeroptions: ["quotes", "curly brackets", "parentheses", "square brackets"], question: "2. The condition in an if / else statement is enclosed within ___."};
+var question_3 = {answer:3, answeroptions: ["numbers & strings", "other arrays", "booleans", "all of the above"], question: "3. Arrays in Javascript can be used to store ___."};
+var question_4 = {answer:2, answeroptions: ["commas", "curly brackets", "quotes", "parentheses"], question: "4. String values must be enclosed within ___ when being assigned to variables."};
+var question_5 = {answer:0, answeroptions: ["JavaScript", "terminal / bash", "for leaps", "console.log"], question: "5. A very useful tool used during development and debugging for printing content to the debugger is:"};
+
+
+var global_questions_array = [question_1, question_2, question_3, question_4, question_5];
+
+
+function startQuiz()
+{
+	setTime();
+	generateHtml();
 }
 
-function pageThree() {
-    var pageThreeEl = document.getElementById("pagethree");
-    if (pageThreeEl.style.display = "none") {
-        pageThreeEl.style.display = "block";
-    } else {
-        pageThreeEl.style.display = "none";
-    }
-    var pageTwoEl = document.getElementById("pagetwo");
-    if (pageTwoEl.style.display = "block") {
-        pageTwoEl.style.display = "none";
-    }
+function nextPage()
+{
+	current_page_number++; //increase page.
+
+
+	if (typeof global_questions_array[current_page_number] == "object" && typeof global_questions_array[current_page_number].answeroptions == "object") //exists in array above.
+		{
+			generateHtml();
+		}
+		else
+		{
+			//assume we have reached the end.
+			generateResultPageHtml();
+		}
+
 }
 
-function pageFour() {
-    var pageFourEl = document.getElementById("pagefour");
-    if (pageFourEl.style.display = "none") {
-        pageFourEl.style.display = "block";
-    } else {
-        pageFourEl.style.display = "none";
-    }
-    var pageThreeEl = document.getElementById("pagethree");
-    if (pageThreeEl.style.display = "block") {
-        pageThreeEl.style.display = "none";
-    }
+
+
+function create_button_helper(button_text, is_correct)
+{
+//we want to create:
+/*
+                        <li>
+                            <button class="btn" onclick="DOSOMETHING()">3. quotes</button>
+                        </li>
+*/
+
+var li_element = document.createElement('li');
+var el = document.createElement('button');
+if(is_correct == true)
+	{
+el.addEventListener('click', handle_button_click_correct);
+	}
+	else
+	{
+el.addEventListener('click', handle_button_click_wrong);
+	}
+el.className="btn";
+el.textContent = button_text;
+
+li_element.appendChild(el); //we now have <li><button></li>
+//append it into the ul:
+
+
+pageanswersEl.appendChild(li_element);
+
+
 }
 
-function pageFive() {
-    var pageFiveEl = document.getElementById("pagefive");
-    if (pageFiveEl.style.display = "none") {
-        pageFiveEl.style.display = "block";
-    } else {
-        pageFiveEl.style.display = "none";
-    }
-    var pageFourEl = document.getElementById("pagefour");
-    if (pageFourEl.style.display = "block") {
-        pageFourEl.style.display = "none";
-    }
+
+
+function handle_button_click_wrong(event)
+{
+resultareaEl.innerHTML="Wrong!";
+setTimeout(nextPage, 1100);
+  console.log('element clickedAAA', event);
 }
 
-function pageSix() {
-    var pageSixEl = document.getElementById("pagesix");
-    if (pageSixEl.style.display = "none") {
-        pageSixEl.style.display = "block";
-    } else {
-        pageSixEl.style.display = "none";
-    }
-    var pageFiveEl = document.getElementById("pagefive");
-    if (pageFiveEl.style.display = "block") {
-        pageFiveEl.style.display = "none";
-    }
+
+function handle_button_click_correct(event)
+{
+resultareaEl.innerHTML="Correct!!!";
+global_score++; // increase score.
+setTimeout(nextPage, 1100);
+  console.log('element clickedBBB', event);
 }
 
-function pageSeven() {
-    var pageSevenEl = document.getElementById("pageseven");
-    if (pageSevenEl.style.display = "none") {
-        pageSevenEl.style.display = "block";
-    } else {
-        pageSevenEl.style.display = "none";
-    }
-    var pageSixEl = document.getElementById("pagesix");
-    if (pageSixEl.style.display = "block") {
-        pageSixEl.style.display = "none";
-    }
+
+
+
+function generateHtml()
+{
+
+if (typeof global_questions_array[current_page_number] != "object" || typeof global_questions_array[current_page_number].answeroptions != "object")
+	{
+		alert("Failed to generate html for page: " + current_page_number);
+		return false;
+	}
+
+
+
+pageoneEl.style.display="none"; //Hide pageone if it happens to be visibkle.
+questionanswerareaEl.style.display="block"; //Make our question/answer area visible
+
+
+
+pagequestionEl.innerHTML=global_questions_array[current_page_number].question;
+resultareaEl.innerHTML=""; //clear the Wrong/Correct area.
+pageanswersEl.innerHTML=""; // reset the answer area content, we append the buttons into it below:
+
+
+var is_correct;
+var answer_prefix = 0;
+var answer_text;
+for(item in global_questions_array[current_page_number].answeroptions)
+	{
+		if(item == global_questions_array[current_page_number].answer)
+		{
+			is_correct = true; //the answer we are doing now is the right one.
+		}
+		else
+		{
+			is_correct = false;
+		}
+		answer_prefix = (item / 1) + 1; // ie so that the first item which is 0 becomes 1.
+		answer_text = answer_prefix + ". " + global_questions_array[current_page_number].answeroptions[item];
+		create_button_helper(answer_text, is_correct);
+	}
+
+
+return true;
 }
 
-function pageEight() {
-    var pageEightEl = document.getElementById("pageeight");
-    if (pageEightEl.style.display = "none") {
-        pageEightEl.style.display = "block";
-    } else {
-        pageEightEl.style.display = "none";
-    }
-    var pageSevenEl = document.getElementById("pageseven");
-    if (pageSevenEl.style.display = "block") {
-        pageSevenEl.style.display = "none";
-    }
+
+function generateResultPageHtml()
+{
+
+questionanswerareaEl.style.display="none"; //Hide question and answer area
+highscoreareaEl.style.display="block"; //Hide question and answer area
+finalscoreEl.innerHTML=global_score;
 }
+
+
 
 function setTime() {
     var timerInterval = setInterval(function() {
@@ -118,44 +169,5 @@ function setTime() {
   }, 1000);
 }
 
-function goPageTwo() {
-    setTime();
-    pageTwo();
-}
 
-startQuizEl.addEventListener("click", goPageTwo);
 
-function goPageThree() {
-    pageThree();
-}
-
-pageTwoT.addEventListener("click", goPageThree);
-pageTwoF.addEventListener("click", goPageThree);
-
-function goPageFour() {
-    pageFour();
-}
-
-pageThreeT.addEventListener("click", goPageFour);
-pageThreeF.addEventListener("click", goPageFour);
-
-function goPageFive() {
-    pageFive();
-}
-
-pageFourT.addEventListener("click", goPageFive);
-pageFourF.addEventListener("click", goPageFive);
-
-function goPageSix() {
-    pageSix();
-}
-
-pageFiveT.addEventListener("click", goPageSix);
-pageFiveF.addEventListener("click", goPageSix);
-
-function goPageSeven() {
-    pageSeven();
-}
-
-pageSixT.addEventListener("click", goPageSeven);
-pageSixF.addEventListener("click", goPageSeven);
